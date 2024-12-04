c70f23041810b969dd154c7a33352d714db2db5b
RaghavsScarletSplendour <72269666+RaghavsScarletSplendour@users.noreply.github.com>
2024-12-04 13:17:15 -0800

    Web3 front end, object creation

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,27 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    string public candidate;
+    // Store candidates
+    // Fetch candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store candidate count
+    uint public candidatesCount;
 
     constructor() public{
-        candidate = "Candidate 1";
+        addCandidate(("Candidate 1"));
+        addCandidate(("Candidate 2"));
+    }
+
+    function addCandidate (string memory _name) private  {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
